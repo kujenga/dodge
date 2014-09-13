ba8cd80adc7e7f6b3e518cace3a522421b89cab8
kujenga <ataylor0123@gmail.com>
2014-09-13 20:29:57 +0200

    added game types and a selection screen for them

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 A game for written for class at AIT Budapest.
+
+Uses older, deprecated code from OpenGL and GLUT, but is fine for learning purposes.
--- dodge/main.cpp
+++ dodge/main.cpp
@@ -6,8 +6,6 @@
 //  Copyright (c) 2014 Aaron Taylor. All rights reserved.
 //
 
-#define ARC4RANDOM_MAX      0x100000000
-
 #define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdio.h>
@@ -25,18 +23,42 @@
 // http://www.opengl.org/resources/libraries/glut/
 #include <GLUT/glut.h>
 
+#define ARC4RANDOM_MAX 0x100000000
+// random double between 0.0 and 1.0
+#define LITTLE_RAND ((double)arc4random() / ARC4RANDOM_MAX)
+
 //////////////////////////////
 // game tuning parameters
 //////////////////////////////
 
-#define NUM_BLOCKS 40
+#define NUM_BLOCKS 45
 #define STEP_SIZE 0.02
 #define MOUSE_WEIGHT 25.0
 #define COLLISION_DIST 0.07
 
 //////////////////////////////
-// global variables for objects
+// call back method definitions
 //////////////////////////////
+
+void onReshape(int winWidth0, int winHeight0);
+void onKeyboard(unsigned char key, int x, int y);
+void onMotion(int x, int y);
+void onIdle();
+void onDisplay();
+
+//////////////////////////////
+// global variables and types
+//////////////////////////////
+
+typedef enum{
+    selection,
+    straightGame,
+    diagonalGame,
+    sinGame,
+    jitterGame
+}mode;
+
+mode gameMode = selection;
 
 int score = 0;
 
@@ -45,12 +67,14 @@ int score = 0;
 //////////////////////////////
 
 class Block {
+    float baseYOffset = 0.5; // allows for centered behavior such as sine curves
     float yOffset = 0.5;
     float xOffset = -1.0;
     
     float size = 0.05;
     
     float speedMultiplier = 1.0;
+    float verticalMovement = 0.0;
     
     float rColor = 0.0;
     float gColor = 0.0;
@@ -60,16 +84,41 @@ public:
         this->setRandValues();
         xOffset += 1.0;
     }
-    void incX(float i) { xOffset += i * speedMultiplier; }
+    void moveX(float i)
+    {
+        xOffset += i * speedMultiplier;
+        switch (gameMode) {
+            case straightGame:
+                // nothing to do
+                break;
+            case diagonalGame:
+                yOffset += verticalMovement/10.0;
+                break;
+            case sinGame:
+                yOffset = baseYOffset + sin(xOffset*10)*verticalMovement*30.0;
+                break;
+            case jitterGame:
+                // 1/10 chance that it will change direction
+                if (rand()%15 == 0) {
+                    verticalMovement = -1 * verticalMovement;
+                }
+                yOffset = yOffset + 0.8*verticalMovement;
+                
+            default:
+                break;
+        }
+    }
     void setRandValues()
     {
-        yOffset = ((double)arc4random() / ARC4RANDOM_MAX) * 1.5 - 0.5;
-        xOffset = ((double)arc4random() / ARC4RANDOM_MAX) * 3.0 - 4.0;
-        size = ((double)arc4random() / ARC4RANDOM_MAX) * 0.07 + 0.02;
-        speedMultiplier = ((double)arc4random() / ARC4RANDOM_MAX) * 0.5 + 0.8;
-        rColor = ((double)arc4random() / ARC4RANDOM_MAX);
-        gColor = ((double)arc4random() / ARC4RANDOM_MAX);
-        bColor = ((double)arc4random() / ARC4RANDOM_MAX);
+        baseYOffset = LITTLE_RAND * 2.0 - 1.0;
+        yOffset = baseYOffset;
+        xOffset = LITTLE_RAND * 3.0 - 4.0;
+        size = LITTLE_RAND * 0.07 + 0.02;
+        speedMultiplier = LITTLE_RAND * 0.5 + 0.8;
+        verticalMovement = LITTLE_RAND * 0.01 - 0.005;
+        rColor = LITTLE_RAND;
+        gColor = LITTLE_RAND;
+        bColor = LITTLE_RAND;
     }
     void draw()
     {
@@ -139,7 +188,7 @@ public:
 bool showPlayer = false;
 bool newPlayer = false;
 
-bool alive = true;
+bool alive = false;
 
 float weightedMousePos = 0.0;
 float curMousePos = 0.0;
@@ -151,8 +200,56 @@ Block *blocks[NUM_BLOCKS];
 Player *player;
 
 //////////////////////////////
+// game actions
+//////////////////////////////
+
+void returnToLife( )
+{
+    if (alive) {
+        return;
+    }
+    
+    alive = true;
+    score = 0;
+    for (int i = 0; i < NUM_BLOCKS; i++) {
+        blocks[i]->setRandValues();
+        blocks[i]->moveX(0.5);
+    }
+}
+
+//////////////////////////////
 // drawing code
 //////////////////////////////
+
+void drawSelection( )
+{
+    for (int i = 1; i <= 4; i++) {
+        // draw each of the blocks
+    }
+    glBegin(GL_POLYGON);
+    
+    glColor3d(0.0f, 0.0f, 0.0f);
+    glVertex2d(-0.7, 0.3);
+    glVertex2d(0.7, 0.3);
+    glVertex2d(0.7, -0.3);
+    glVertex2d(-0.7, -0.3);
+    
+    glEnd();
+    
+    char buf[BUFSIZ];
+    char name[] = "Straight:1 Diagonal:2\n Sine:3 Jitter:4";
+    
+    snprintf(buf, sizeof(buf), "%s", name);
+    
+    //printf("chars: %s\n",buf);
+    //int len = (int)strlen(buf);
+    glColor3d(1.0f, 1.0f, 1.0f);
+    glPushMatrix();
+    glRasterPos2f(-0.55,0.0);
+    for (char *p = buf; *p; p++)
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *p);
+    glPopMatrix();
+}
 
 void drawGround( )
 {
@@ -183,11 +280,13 @@ void drawScore( )
     char buf[BUFSIZ];
     char name[] = "Score";
     
-    snprintf(buf, sizeof(buf), "%s: %010d", name, score);
+    snprintf(buf, sizeof(buf), "%s: %08d", name, score);
     
+    //printf("chars: %s\n",buf);
     //int len = (int)strlen(buf);
     glPushMatrix();
-    glTranslatef(0.5, 0.5, 0.0);
+    glRasterPos2f(-0.97,0.9);
+    glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
     for (char *p = buf; *p; p++)
         glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, *p);
     glPopMatrix();
@@ -197,59 +296,6 @@ void drawScore( )
 // callback functions
 //////////////////////////////
 
-void onIdle( ) {
-    
-    // time elapsed since program started, in seconds
-    double t = glutGet(GLUT_ELAPSED_TIME) * 0.001;
-    // variable to remember last time idle was called
-    static double lastTime = 0.0;
-    // time difference between calls: time step
-    double dt = t - lastTime;
-    // store time
-    if (alive) {
-        lastTime = t;
-    } else {
-        gameStart = t;
-    }
-    
-    // base speed of 0.2 units/sec, increases as game progresses
-    float inc = dt * 0.2 + 0.01 * (t - gameStart);
-    
-    for (int i = 0; alive && i < NUM_BLOCKS; i++)
-    {
-        blocks[i]->incX(dt * inc);
-    }
-    
-    weightedMousePos = ((weightedMousePos * (MOUSE_WEIGHT - 1)) + curMousePos) / MOUSE_WEIGHT;
-    
-    drawScore();
-    // show
-    glutPostRedisplay();
-}
-
-
-void onDisplay( )
-{
-    // draw here
-    glClearColor(0.4f, 0.7f, 0.9f, 0.5f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    
-    drawGround();
-    drawScore();
-    
-    for (int i = 0; i < NUM_BLOCKS; i++) {
-        if (blocks[i]->hasCollided(0.9, player->yOffset)) {
-            alive = false;
-        } else {
-            blocks[i]->draw();
-        }
-        
-    }
-    player->draw(weightedMousePos);
-    
-    glutSwapBuffers();
-}
-
 void onReshape(int winWidth0, int winHeight0)
 {
     glViewport(0, 0, winWidth0, winHeight0);
@@ -257,15 +303,44 @@ void onReshape(int winWidth0, int winHeight0)
 
 void onKeyboard(unsigned char key, int x, int y)
 {
+    // if the game is happening
     switch (key) {
-        // restart the game
+            // restart the game
         case 'r':
-            alive = true;
+            returnToLife();
             break;
             
-        // pause the game
+            // pause the game
         case 'p':
             // do something helpful
+            break;
+            
+        case '1':
+            gameMode = straightGame;
+            if (!alive) {
+                returnToLife();
+            }
+            break;
+            
+        case '2':
+            gameMode = diagonalGame;
+            if (!alive) {
+                returnToLife();
+            }
+            break;
+            
+        case '3':
+            gameMode = sinGame;
+            if (!alive) {
+                returnToLife();
+            }
+            break;
+            
+        case '4':
+            gameMode = jitterGame;
+            if (!alive) {
+                returnToLife();
+            }
             break;
             
         default:
@@ -285,6 +360,67 @@ void onMotion(int x, int y)
     }
     weightedMousePos = ((weightedMousePos * (MOUSE_WEIGHT - 1)) + curMousePos) / MOUSE_WEIGHT;
     //printf("cur: %f\nscore: %i\n",curMousePos,score);
+}
+
+void onIdle()
+{
+    // time elapsed since program started, in seconds
+    double t = glutGet(GLUT_ELAPSED_TIME) * 0.001;
+    
+    // variable to remember last time idle was called
+    static double lastTime = 0.0;
+    // time difference between calls: time step
+    double dt = t - lastTime;
+    // store time
+    if (alive) {
+        lastTime = t;
+    } else {
+        gameStart = t;
+    }
+    
+    // base speed of 0.2 units/sec, increases as game progresses
+    float inc = dt * 0.2 + 0.01 * (t - gameStart);
+    for (int i = 0; alive && i < NUM_BLOCKS; i++)
+    {
+        blocks[i]->moveX(dt * inc);
+    }
+    
+    weightedMousePos = ((weightedMousePos * (MOUSE_WEIGHT - 1)) + curMousePos) / MOUSE_WEIGHT;
+    
+    // show
+    glutPostRedisplay();
+}
+
+
+void onDisplay()
+{
+    // draw here
+    glClearColor(0.4f, 0.7f, 0.9f, 0.5f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    
+    drawGround();
+    
+    if (gameMode == selection) {
+        // show
+        drawSelection();
+        
+        glutSwapBuffers();
+        return;
+    }
+
+    drawScore();
+    
+    for (int i = 0; i < NUM_BLOCKS; i++) {
+        if (blocks[i]->hasCollided(0.9, player->yOffset)) {
+            alive = false;
+        } else {
+            blocks[i]->draw();
+        }
+        
+    }
+    player->draw(weightedMousePos);
+    
+    glutSwapBuffers();
 }
 
 //////////////////////////////
